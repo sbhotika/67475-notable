d1fa6b1eec755c29568285aa479678b35954a66d
Alec Lam <alechunterlam@gmail.com>
2018-11-21 13:31:10 -0500

    Implemented voice recognition demo

--- Notable/db/migrate/20181115183720_create_user_notes.rb
+++ Notable/db/migrate/20181115183720_create_user_notes.rb
@@ -1,8 +1,8 @@
 class CreateUserNotes < ActiveRecord::Migration
   def change
     create_table :user_notes do |t|
-      t.int :user_id
-      t.int :note_id
+      t.integer :user_id
+      t.integer :note_id
       t.boolean :is_favorite
 
       t.timestamps null: false
